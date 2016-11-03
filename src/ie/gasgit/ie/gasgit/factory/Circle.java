db69d5ed63474358552e92f1b3ed2a20173f9dae
gasgit <g00305555@gmit.ie>
2016-11-03 00:56:46 +0000

    factory

---
+++ src/ie/gasgit/ie/gasgit/factory/Circle.java
@@ -0,0 +1,12 @@
+package ie.gasgit.ie.gasgit.factory;
+
+/**
+ * Created by glen on 02/08/16.
+ */
+public class Circle implements Shape{
+
+    @Override
+    public void draw() {
+        System.out.println("Drawing A Circle");
+    }
+}
---
+++ src/ie/gasgit/ie/gasgit/factory/Square.java
@@ -0,0 +1,11 @@
+package ie.gasgit.ie.gasgit.factory;
+
+/**
+ * Created by glen on 02/08/16.
+ */
+public class Square implements Shape {
+    @Override
+    public void draw() {
+        System.out.println("Drawing A Square");
+    }
+}
